6476079d3b8dfa6ad7f4b1959a2b2d762dc8d8e1
Arvind Rao <arvindrao.759@gmail.com>
2022-09-11 13:02:39 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import MyComponent from "./src/MyComponent";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MyComponent />
+      {/* <Text fontSize:string="18">Hello</Text> */}
+      <MaterialIcons name="keyboard-arrow-right" size={50} color="blue" />
+      <MaterialIcons name="keyboard-return" size={100} color="red" />
+      <MaterialIcons name="laptop-windows" size={85} color="black" />
+      <MaterialIcons name="library-add" size={100} color="violet" />
+      <Text>Namskara</Text>
     </View>
   );
 }
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/MyComponent.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Appbar, Provider as PaperProvider } from "react-native-paper";
+import { Platform } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Text, View } from "react-native";
+
+const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
+
+const MyComponent = () => (
+  <Appbar.Header>
+    <Appbar.Content title="Title" subtitle={"Subtitle"} />
+    <Appbar.Action icon="magnify" onPress={() => {}} />
+    <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+    <Text>From MyComponent</Text>
+    <Ionicons name="md-checkmark-circle" size={32} color="green" />
+  </Appbar.Header>
+);
+
+export default MyComponent;
